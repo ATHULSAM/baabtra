74520f3a9a4d08b5f33278ddc01600b5420ca1de
athul <athulsam10@gmail.com>
2021-02-05 13:29:53 +0530

    yy commit

--- newapp/urls.py
+++ newapp/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('test2',views.test2fun,name="test2"),
     path('test3',views.test3fun,name="test3"),
     path('sample',views.samplefun),
+    path('sample1',views.samfun,name="sample1"),
+    path('form',views.formfun,name="form"),
+    path('fb',views.fbfun,name="fb"),
+    path('sample2',views.sample2fun,name="sample2"),
+    
+    
 
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -14,3 +14,17 @@ def test3fun(request):
     return render(request,'assignment3.html')  
 def samplefun(request):
     return render(request,'sample.html')  
+def samfun(request):
+    return render(request,'bs.html')    
+def formfun(request):
+    return render(request,'form.html')    
+def fbfun(request):
+    return render(request,'fb.html')  
+def pjtfun(request):
+    return render(request,'pjt.html')
+def jsfun(request):
+    return render(request,'js.html')    
+def sample2fun(request):
+    return render(request,'assignment5.html')      
+
+
